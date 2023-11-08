3fac01f030e11a9ea45bb5ccba6d898b8f273bfe
galihagussetyawan <galihdevelopment@gmail.com>
2023-11-08 16:05:54 +0700

    feat: create API endpoint for create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { CreateUserRequest, UserService } from './user.service';
+import { Response } from 'express';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Res() res: Response, @Body() reqBody: CreateUserRequest) {
+    try {
+      res.status(HttpStatus.OK).send({
+        status: HttpStatus.OK,
+        message: 'success create user',
+        data: await this.userService.createUser(reqBody),
+      });
+    } catch (error) {
+      res.status(error.status).send({
+        status: error.status,
+        message: error.message,
+      });
+    }
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+
+export type CreateUserRequest = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(req: CreateUserRequest): Promise<User> {
+    return await this.userRepository.save(req);
+  }
+}
